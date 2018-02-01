986171194a767429c28922154cbcd5aa09311832
Antoineone <antoineone01@gmail.com>
2018-02-01 16:54:00 +0100

    Structure de base avec Flask

--- index.py
+++ index.py
@@ -1,1 +1,26 @@
-#Page d'accueil
+from flask import Flask
+
+app = Flask(__name__)
+
+
+# Page d'accueil
+@app.route('/')
+def index():
+    return "Accueil"
+
+
+# Page de connection
+@app.route('/login')
+def connection():
+    return "connection"
+
+
+# Page d'inscription
+@app.route('/register')
+def inscription():
+    return "inscription"
+
+
+# Lancement du serveur lors de l'exécution du fichier
+if __name__ == '__main__':
+    app.run()
