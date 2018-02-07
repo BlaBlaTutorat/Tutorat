ea91060590bf2a1f311d4941b547aeaf45a9e926
Antoineone <antoineone01@gmail.com>
2018-02-07 20:08:34 +0100

    Mise en page et lien des pages avec Flask

--- index.py
+++ index.py
@@ -21,6 +21,24 @@ def inscription():
     return render_template("inscription.html")
 
 
+# Profil
+@app.route('/profil')
+def profil():
+    return "Mon profil"
+
+
+# Mot de passe oublié
+@app.route('/forgot')
+def mdp_oublie():
+    return render_template("mdp_oublie.html")
+
+
+# Page de validation
+@app.route('/validate')
+def validation():
+    return render_template("validation.html")
+
+
 # Lancement du serveur lors de l'exécution du fichier
 if __name__ == '__main__':
     app.run()
