1f59662da6289b676612ec78d85abde8edf22e8f
Taotom <35769197+Taotom@users.noreply.github.com>
2018-02-12 21:40:27 +0100

    Update tutorat.sql

--- tutorat.sql
+++ tutorat.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  lun. 12 fév. 2018 à 12:59
+-- Généré le :  lun. 12 fév. 2018 à 20:39
 -- Version du serveur :  5.7.19
 -- Version de PHP :  5.6.31
 
@@ -13,9 +13,9 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT = @@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS = @@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION = @@COLLATION_CONNECTION */;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
 --
@@ -30,22 +30,19 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `classes`;
 CREATE TABLE IF NOT EXISTS `classes` (
-  `id`      INT(11)     NOT NULL AUTO_INCREMENT,
-  `niveau`  VARCHAR(50) NOT NULL,
-  `filiere` VARCHAR(15) NOT NULL,
-  `numero`  INT(11)     NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `niveau` varchar(50) NOT NULL,
+  `filiere` varchar(15) NOT NULL,
+  `numero` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-)
-  ENGINE = InnoDB
-  AUTO_INCREMENT = 2
-  DEFAULT CHARSET = utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `classes`
 --
 
 INSERT INTO `classes` (`id`, `niveau`, `filiere`, `numero`) VALUES
-  (1, 'Terminale', 'S', 5);
+(1, 'Terminale', 'S', 5);
 
 -- --------------------------------------------------------
 
@@ -55,34 +52,31 @@ INSERT INTO `classes` (`id`, `niveau`, `filiere`, `numero`) VALUES
 
 DROP TABLE IF EXISTS `filieres`;
 CREATE TABLE IF NOT EXISTS `filieres` (
-  `id`  INT(11)     NOT NULL AUTO_INCREMENT,
-  `nom` VARCHAR(50) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-)
-  ENGINE = InnoDB
-  AUTO_INCREMENT = 16
-  DEFAULT CHARSET = utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `filieres`
 --
 
 INSERT INTO `filieres` (`id`, `nom`) VALUES
-  (1, 'Aucune filière (2nd)'),
-  (2, 'S'),
-  (3, 'ES'),
-  (4, 'L'),
-  (5, 'MPSI'),
-  (6, 'PCSI'),
-  (7, 'MP'),
-  (8, 'MP*'),
-  (9, 'PSI'),
-  (10, 'PC'),
-  (11, 'PC*'),
-  (12, 'BCPST'),
-  (13, 'ECE'),
-  (14, 'ECS'),
-  (15, 'HK/K');
+(1, 'Aucune filière (2nd)'),
+(2, 'S'),
+(3, 'ES'),
+(4, 'L'),
+(5, 'MPSI'),
+(6, 'PCSI'),
+(7, 'MP'),
+(8, 'MP*'),
+(9, 'PSI'),
+(10, 'PC'),
+(11, 'PC*'),
+(12, 'BCPST'),
+(13, 'ECE'),
+(14, 'ECS'),
+(15, 'HK/K');
 
 -- --------------------------------------------------------
 
@@ -92,43 +86,41 @@ INSERT INTO `filieres` (`id`, `nom`) VALUES
 
 DROP TABLE IF EXISTS `matieres`;
 CREATE TABLE IF NOT EXISTS `matieres` (
-  `nom` VARCHAR(50) NOT NULL,
+  `nom` varchar(50) NOT NULL,
   PRIMARY KEY (`nom`)
-)
-  ENGINE = InnoDB
-  DEFAULT CHARSET = utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `matieres`
 --
 
 INSERT INTO `matieres` (`nom`) VALUES
-  ('(Spé) Grec'),
-  ('(Spé) Latin'),
-  ('Allemand'),
-  ('Anglais'),
-  ('CAV'),
-  ('Chinois'),
-  ('Espagnol'),
-  ('Français'),
-  ('Histoire-Géographie'),
-  ('ISN'),
-  ('Littérature'),
-  ('Littérature en langue vivante étrangère'),
-  ('LVA'),
-  ('Mathèmatiques'),
-  ('Philosophie'),
-  ('Physique-Chime'),
-  ('Russe'),
-  ('Sciences'),
-  ('SES'),
-  ('SI'),
-  ('Spé Eco'),
-  ('Spé Maths'),
-  ('Spé Physique'),
-  ('Spé Sciences sociales et politiques'),
-  ('Spé SVT'),
-  ('SVT');
+('(Spé) Grec'),
+('(Spé) Latin'),
+('Allemand'),
+('Anglais'),
+('CAV'),
+('Chinois'),
+('Espagnol'),
+('Français'),
+('Histoire-Géographie'),
+('ISN'),
+('Littérature'),
+('Littérature en langue vivante étrangère'),
+('LVA'),
+('Mathèmatiques'),
+('Philosophie'),
+('Physique-Chime'),
+('Russe'),
+('Sciences'),
+('SES'),
+('SI'),
+('Spé Eco'),
+('Spé Maths'),
+('Spé Physique'),
+('Spé Sciences sociales et politiques'),
+('Spé SVT'),
+('SVT');
 
 -- --------------------------------------------------------
 
@@ -138,24 +130,21 @@ INSERT INTO `matieres` (`nom`) VALUES
 
 DROP TABLE IF EXISTS `niveaux`;
 CREATE TABLE IF NOT EXISTS `niveaux` (
-  `id`  INT(11)     NOT NULL AUTO_INCREMENT,
-  `nom` VARCHAR(50) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-)
-  ENGINE = InnoDB
-  AUTO_INCREMENT = 6
-  DEFAULT CHARSET = utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `niveaux`
 --
 
 INSERT INTO `niveaux` (`id`, `nom`) VALUES
-  (1, 'Seconde'),
-  (2, 'Première'),
-  (3, 'Terminale'),
-  (4, 'CPGE 1ère année'),
-  (5, 'CPGE 2ème année');
+(1, 'Seconde'),
+(2, 'Première'),
+(3, 'Terminale'),
+(4, 'CPGE 1ère année'),
+(5, 'CPGE 2ème année');
 
 -- --------------------------------------------------------
 
@@ -165,63 +154,62 @@ INSERT INTO `niveaux` (`id`, `nom`) VALUES
 
 DROP TABLE IF EXISTS `offres`;
 CREATE TABLE IF NOT EXISTS `offres` (
-  `id`           INT(11)     NOT NULL AUTO_INCREMENT,
-  `auteur`       VARCHAR(50) NOT NULL,
-  `niveau`       VARCHAR(30) NOT NULL,
-  `filiere`      VARCHAR(30) NOT NULL,
-  `matiere`      VARCHAR(30) NOT NULL,
-  `date_time`    DATETIME    NOT NULL
-  COMMENT 'date de la demande',
-  `participant`  VARCHAR(50)          DEFAULT NULL,
-  `participant2` VARCHAR(50)          DEFAULT NULL,
-  `disponible`   TINYINT(1)  NOT NULL DEFAULT '0',
-  `debut_j1`     TIME                 DEFAULT NULL,
-  `fin_j1`       TIME                 DEFAULT NULL,
-  `debut_j2`     TIME                 DEFAULT NULL,
-  `fin_j2`       TIME                 DEFAULT NULL,
-  `debut_j3`     TIME                 DEFAULT NULL,
-  `fin_j3`       TIME                 DEFAULT NULL,
-  `debut_j4`     TIME                 DEFAULT NULL,
-  `fin_j4`       TIME                 DEFAULT NULL,
-  `debut_j5`     TIME                 DEFAULT NULL,
-  `fin_j5`       TIME                 DEFAULT NULL,
-  `debut_j6`     TIME                 DEFAULT NULL,
-  `fin_j6`       TIME                 DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `auteur` varchar(50) NOT NULL,
+  `niveau` varchar(30) NOT NULL,
+  `filiere` varchar(30) NOT NULL,
+  `matiere` varchar(30) NOT NULL,
+  `date_time` datetime NOT NULL COMMENT 'date de la demande',
+  `participant` varchar(50) DEFAULT NULL,
+  `participant2` varchar(50) DEFAULT NULL,
+  `disponible` tinyint(1) NOT NULL DEFAULT '0',
+  `debut_j1` time DEFAULT NULL,
+  `fin_j1` time DEFAULT NULL,
+  `debut_j2` time DEFAULT NULL,
+  `fin_j2` time DEFAULT NULL,
+  `debut_j3` time DEFAULT NULL,
+  `fin_j3` time DEFAULT NULL,
+  `debut_j4` time DEFAULT NULL,
+  `fin_j4` time DEFAULT NULL,
+  `debut_j5` time DEFAULT NULL,
+  `fin_j5` time DEFAULT NULL,
+  `debut_j6` time DEFAULT NULL,
+  `fin_j6` time DEFAULT NULL,
   PRIMARY KEY (`id`)
-)
-  ENGINE = InnoDB
-  AUTO_INCREMENT = 7
-  DEFAULT CHARSET = utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `offres`
 --
 
-INSERT INTO `offres` (`id`, `auteur`, `niveau`, `filiere`, `matiere`, `date_time`, `participant`, `participant2`, `disponible`, `debut_j1`, `fin_j1`, `debut_j2`, `fin_j2`, `debut_j3`, `fin_j3`, `debut_j4`, `fin_j4`, `debut_j5`, `fin_j5`, `debut_j6`, `fin_j6`)
-VALUES
-  (1, 'Marco Desmoulins', 'Terminale', 'S', 'Mathématiques', '2018-02-22 00:00:00', NULL, NULL, 1, NULL, NULL, NULL,
-                                                                                                               NULL,
-                                                                                                               NULL,
-                                                                                                               NULL,
-                                                                                                               NULL,
-                                                                                                               NULL,
-                                                                                                               NULL,
-                                                                                                               NULL,
-                                                                                                               NULL,
-                                                                                                               NULL),
-  (2, 'Tao Blancheton', 'Terminale', 'S', 'ISN', '2018-02-19 14:15:41', NULL, NULL, 1, NULL, NULL, NULL, NULL, NULL,
-                                                                                                   NULL, NULL, NULL,
-                                                                                                   NULL, NULL, NULL,
-                                                                                                   NULL),
-  (3, 'Antoine Labarussias', 'Terminale', 'S', 'Science de l\'ingénieur', '2018-02-22 00:00:00', NULL, NULL, 1, NULL,
-      NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-  (4, 'Jean Kévin', 'Seconde', 'Aucune filière (2nd)', 'Français', '2018-02-22 00:00:00', NULL, NULL, 1, NULL, NULL,
-    NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-  (5, 'Alexis Ducont', 'Première', 'L', 'ISN', '2018-02-19 14:15:41', NULL, NULL, 1, NULL, NULL, NULL, NULL, NULL, NULL,
-                                                                                                 NULL, NULL, NULL, NULL,
-                                                                                                 NULL, NULL),
-  (6, 'Lola Blachard', 'Seconde', 'Aucune filière (2nd)', 'Science de l\'ingénieur', '2018-02-22 00:00:00', NULL, NULL,
-      1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `offres` (`id`, `auteur`, `niveau`, `filiere`, `matiere`, `date_time`, `participant`, `participant2`, `disponible`, `debut_j1`, `fin_j1`, `debut_j2`, `fin_j2`, `debut_j3`, `fin_j3`, `debut_j4`, `fin_j4`, `debut_j5`, `fin_j5`, `debut_j6`, `fin_j6`) VALUES
+(1, 'Marco Desmoulins', 'Terminale', 'S', 'Mathématiques', '2018-02-22 00:00:00', NULL, NULL, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(2, 'Tao Blancheton', 'Terminale', 'S', 'ISN', '2018-02-19 14:15:41', NULL, NULL, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(3, 'Antoine Labarussias', 'Terminale', 'S', 'Science de l\'ingénieur', '2018-02-22 00:00:00', NULL, NULL, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(4, 'Jean Kévin', 'Seconde', 'Aucune filière (2nd)', 'Français', '2018-02-22 00:00:00', NULL, NULL, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(5, 'Alexis Ducont', 'Première', 'L', 'ISN', '2018-02-19 14:15:41', NULL, NULL, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(6, 'Lola Blachard', 'Seconde', 'Aucune filière (2nd)', 'Science de l\'ingénieur', '2018-02-22 00:00:00', NULL, NULL, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `specialites`
+--
+
+DROP TABLE IF EXISTS `specialites`;
+CREATE TABLE IF NOT EXISTS `specialites` (
+  `nom` varchar(50) NOT NULL,
+  PRIMARY KEY (`nom`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `specialites`
+--
+
+INSERT INTO `specialites` (`nom`) VALUES
+('Economique et sociale'),
+('Littéraire'),
+('Scientifique');
 
 -- --------------------------------------------------------
 
@@ -231,26 +219,26 @@ VALUES
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
-  `nom`     VARCHAR(50) NOT NULL DEFAULT '',
-  `mdp`     VARCHAR(15) NOT NULL,
-  `mail`    VARCHAR(50) NOT NULL,
-  `niveau`  VARCHAR(15) NOT NULL,
-  `filiere` VARCHAR(15) NOT NULL,
-  `admin`   TINYINT(1)  NOT NULL DEFAULT '0',
-  `ban`     TINYINT(1)  NOT NULL DEFAULT '0',
+  `nom` varchar(50) NOT NULL DEFAULT '',
+  `mdp` varchar(15) NOT NULL,
+  `mail` varchar(50) NOT NULL,
+  `niveau` varchar(15) NOT NULL,
+  `filiere` varchar(15) NOT NULL,
+  `admin` tinyint(1) NOT NULL DEFAULT '0',
+  `ban` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`nom`)
-)
-  ENGINE = InnoDB
-  DEFAULT CHARSET = utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `users`
 --
 
 INSERT INTO `users` (`nom`, `mdp`, `mail`, `niveau`, `filiere`, `admin`, `ban`) VALUES
-  ('Jean Kévin', 'mdp', 'jk@gmail.com', 'Terminale', 'S', 0, 0);
+('Antoine Labarussias', 'antoine', '?', 'Terminale', 'S', 1, 0),
+('Marco Desmoulins', 'marco', '?', 'Terminale', 'S', 1, 0),
+('Tao Blancheton', 'notehcnalb.oat', 'taotom63@gmail.com', 'Terminale', 'S', 1, 0);
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT = @OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS = @OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION = @OLD_COLLATION_CONNECTION */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
