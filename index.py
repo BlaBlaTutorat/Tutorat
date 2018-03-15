2df12c471637d2f77d12bbd6427baecea7e34842
blazingworm <35993430+blazingworm@users.noreply.github.com>
2018-03-15 21:11:16 +0100

    Update index.py

--- index.py
+++ index.py
@@ -25,23 +25,38 @@ def index():
 def connexion():
     if 'mail' not in session:
         sql_obj = sql.MysqlObject()
+        admin_user = True
+        user = "Tao Blancheton"
+        css_state = sql_obj.get_css(user)
+       
         # Propre à cette page
         hidemenu = True
-
-        if request.method == 'POST':
-            # obtenir les données entrées par l'utilisateur
-            # chiffrer le mot de passe
-            # comparer la nouvelle empreinte à celle de la base dde donnée
-            # valider ou non la connexion
-            session['mail'] = request.form['mail']
-            return redirect(url_for('recherche'))
-
         if request.args.get('info_msg'):
             info_msg = request.args.get('info_msg')
-        return render_template("connexion.html", **locals())
+            return render_template("connexion.html", **locals())
+
+        if request.method == 'POST':
+            
+            # obtenir les données entrées par l'utilisateur
+            mail = request.form['mail']
+            mdp = request.form['password']
+            
+            # chiffrer le mot de passe
+            
+            mdp_chiffre = hashlib.sha256(str(mdp)).encode('utf-8').hexdigest()
+            
+            # comparer les infos à celle de la base dde donnée
+            if sql_obj.get_compte(mot_de_passe_chiffre) == mdp_chiffre and sql_obj.get_compte(mail):
+            # valider ou non  la connexion
+                sql_obj.connect(mail,mdp_chiffre)
+                return redirect(url_for('recherche'))
+            else:
+                 return redirect(url_for('connexion', info_msg="Erreur lors de la connexion, veuillez vérifier les informations saises puis réessayez."))
+        
     else:
-        # Redirection si l'utilisateur est connecté
-        return redirect(url_for('recherche'))
+           # Redirection si l'utilisateur est connecté
+      
+          return redirect(url_for('recherche'))
 
 
 # Page d'inscription
