b0c4accf240ee4f7239d69ec5fb94e48683b5517
Antoineone <antoineone01@gmail.com>
2018-04-10 17:07:09 +0200

    Correction

---
+++ static/js/jquery.main.js
@@ -0,0 +1,13 @@
+// Fonction executée lors de la pression d'une touche clavier
+$(document).keydown(function (event) {
+    if (event.keyCode == 123) { // Prevent F12
+        return false;
+    } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) { // Prevent Ctrl+Shift+I
+        return false;
+    }
+});
+
+// Fonction qui désactive le menu affiché lors du clique droit
+$(document).on("contextmenu", function (e) {
+    e.preventDefault();
+});
