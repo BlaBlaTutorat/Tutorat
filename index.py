bc4d2df955210856de94f9c209553ebd930c4fbd
antoine.labarussias <antoinelabarussias@orange.fr>
2018-05-09 09:17:04 +0200

    Corrections

--- index.py
+++ index.py
@@ -525,7 +525,7 @@ def traitement_creation():
         else:
             # Suite du formulaire de création
             horaires = request.form["horaires_data"]
-            if horaires.find("1") > 1:
+            if len([c for c in horaires if c =='1']) > 1:
                 process = True
 
             if process:
