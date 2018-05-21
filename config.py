6eae6cc1ba3967838e41f1d9ba997c5a8de6cd2a
Taotom <35769197+Taotom@users.noreply.github.com>
2018-05-21 19:11:40 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+
+# BDD
+host = ""
+user = ""
+password = ""
+database = "tutorat"
+
+# MAIL
+smtp = "smtp.gmail.com"
+smtp_port = 587
+email = "tutorat.blaisepascal@gmail.com"
+email_password = ""
+
+
+
+
+# A COMPLETER EN PRODUCTION
